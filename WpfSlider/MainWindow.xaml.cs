4457aace50312f694d147eb45bcddf120dbb193c
alex3867 <amuleh@esri.com>
2017-07-03 11:07:33 -0600

    Add project files.

---
+++ WpfSlider/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSlider
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    MyDataContext _myDataContext = new MyDataContext();
+    public MainWindow()
+    {
+      InitializeComponent();
+      DataContext = _myDataContext;
+
+      mySlider.AddHandler(MouseLeftButtonDownEvent, new MouseButtonEventHandler(MySlider_MouseLeftButtonDown), true);
+      mySlider.AddHandler(MouseLeftButtonUpEvent, new MouseButtonEventHandler(MySlider_MouseLeftButtonUp), true);
+    }
+
+    private void MySlider_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+      _myDataContext.IsPressed = false;
+    }
+
+    private void MySlider_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      _myDataContext.IsPressed = true;
+    }
+  }
+
+  public class MyDataContext : INotifyPropertyChanged
+  {
+    private bool _isPressed = false;
+    public bool IsPressed
+    {
+      get
+      {
+        return _isPressed;
+      }
+
+      set
+      {
+        if (value != this._isPressed)
+        {
+          _isPressed = value;
+          NotifyPropertyChanged();
+        }
+      }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
---
+++ WpfSlider/NumberToTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfSlider
+{
+  public sealed class NumberToTimeConverter : IValueConverter
+  {
+    public bool IsReversed { get; set; }
+    public bool UseHidden { get; set; }
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      double number = Math.Round((double)value, 0);
+      double newNumber = number + number;
+      string val = number.ToString() + " + " + number.ToString() + " = " + newNumber.ToString();
+      return val;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
